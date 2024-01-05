a81840c07bbeded560672057c7c550a019a894d5
duydinhthai <dinhthaiduy.tk10@gmail.com>
2024-01-05 12:21:04 +0700

    function flip of octagonal shape
    add fully method for class Octagonal
    and add function for flipping, set time 2seconds then auto flipping backside, delay time still a little bit unexpected, improve next time

--- ZickeZacke_test - Copy/Main/GamePanel.java
+++ ZickeZacke_test - Copy/Main/GamePanel.java
@@ -1,11 +1,17 @@
 package Main;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 import Entity.PLayer;
 import Tile.TileManager;
+import Octagon.Octagon;
 import Octagon.OctagonManager;
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable, ActionListener , MouseListener {
     final int originalTilesize = 16;
     final int scale = 3;
     public final int tileSize = originalTilesize*scale;
@@ -18,7 +24,15 @@ public class GamePanel extends JPanel implements Runnable{
     public final int maprow = 8;
     public final int mapcol = 6;
     public final int octagonSize = 70;
+    public final int octagonHeight = 70;
+    public final int octagonWidth = 70;
+    boolean flip=false;
+    int selected;
+    int count=0;
+    Timer timer;
+
     
+   
 
     int FPS = 10;
 
@@ -31,6 +45,9 @@ public class GamePanel extends JPanel implements Runnable{
     
 
     public GamePanel(){
+        addMouseListener(this);
+        timer = new Timer(50, this);
+        timer.start();
             this.setPreferredSize(new Dimension(screenWidth, screenHeight));
             this.setBackground(Color.BLACK);
             this.setDoubleBuffered(true);
@@ -101,14 +118,76 @@ public class GamePanel extends JPanel implements Runnable{
 
         tileM.draw(g2);
         octagonM.draw(g2);
-        
         player.draw(g2);
-        
-
-
         g2.dispose();
 
     }
+    int direction =1;
+    Octagon[] octagons = OctagonManager.getArray();
+    public void actionPerformed(ActionEvent e) {
+       if(flip)
+       {
+            flip=octagons[selected].flip();
+            if(octagons[selected].getWidth() <=0)
+            {
+                octagons[selected].setPicture(octagons[selected].getName());
+            }
+       }
+       else
+       {
+        try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        octagons[selected].setPicture("backside");
+       }
+
+       repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        flip=true;
+        direction=1;
+
+        for(Octagon oct1: octagons)
+        {
+            if(oct1.collision(e.getX(), e.getY()))
+            {
+                selected=oct1.getIndex();
+            }
+            count++;
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+      
+    }
+
+    // public boolean flip()
+    // {
+    //     width -=30*direction;
+    //     x+=17
+    // }
+
     
     
 }
--- ZickeZacke_test - Copy/Octagon/OctagonManager.java
+++ ZickeZacke_test - Copy/Octagon/OctagonManager.java
@@ -12,10 +12,14 @@ import javax.imageio.ImageIO;
 import Main.GamePanel;
 public class OctagonManager {
     GamePanel gp;
-    Octagon[] octagons;
+    public static Octagon[] octagons;
     int mapOctagonNum[][];
     Octagon backside;
     Octagon backside2;
+    public static Octagon[] getArray()
+    {
+        return octagons;
+    }
     public static void shuffleArray(Octagon[] array) {
         Random random = new Random();
         int n = array.length;
@@ -40,50 +44,92 @@ public class OctagonManager {
     }
 
     public void getOctagonImage(){
+        String[] name={"chick", "connhim", "eggs", "feather", "flower", "hotvitlon", "omelette", "rabbit", "sadchick", "snail", "worm", "worm7mau"};
         try {
-            octagons[0] = new Octagon();
-            octagons[0].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/chick.png"));
+            // octagons[0] = new Octagon();
+            // octagons[0].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/chick.png"));
 
-            octagons[1] = new Octagon();
-            octagons[1].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/chick.png"));
+            // octagons[1] = new Octagon();
+            // octagons[1].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/chick.png"));
 
-            octagons[2] = new Octagon();
-            octagons[2].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/connhim.png"));
+            // octagons[2] = new Octagon();
+            // octagons[2].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/connhim.png"));
 
-            octagons[3] = new Octagon();
-            octagons[3].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/eggs.png"));
+            // octagons[3] = new Octagon();
+            // octagons[3].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/eggs.png"));
 
-            octagons[4] = new Octagon();
-            octagons[4].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/feather.png"));
+            // octagons[4] = new Octagon();
+            // octagons[4].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/feather.png"));
 
-            octagons[5] = new Octagon();
-            octagons[5].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/flower.png"));
+            // octagons[5] = new Octagon();
+            // octagons[5].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/flower.png"));
 
-            octagons[6] = new Octagon();
-            octagons[6].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/hotvitlon.png"));
+            // octagons[6] = new Octagon();
+            // octagons[6].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/hotvitlon.png"));
 
-            octagons[7] = new Octagon();
-            octagons[7].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/omelette.png"));
+            // octagons[7] = new Octagon();
+            // octagons[7].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/omelette.png"));
 
-            octagons[8] = new Octagon();
-            octagons[8].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/rabbit.png"));
+            // octagons[8] = new Octagon();
+            // octagons[8].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/rabbit.png"));
 
-            octagons[9] = new Octagon();
-            octagons[9].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/sadchick.png"));
+            // octagons[9] = new Octagon();
+            // octagons[9].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/sadchick.png"));
 
-            octagons[10] = new Octagon();
-            octagons[10].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/snail.png"));
+            // octagons[10] = new Octagon();
+            // octagons[10].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/snail.png"));
 
-            octagons[11] = new Octagon();
-            octagons[11].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/worm.png"));
+            // octagons[11] = new Octagon();
+            // octagons[11].image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/worm.png"));
 
-            backside = new Octagon();
+            backside = new Octagon("backside",12);
             backside.image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/backside.png"));
 
-            backside2 = new Octagon();
-            backside2.image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/backside2.png"));
+            // backside2 = new Octagon();
+            // backside2.image = ImageIO.read(getClass().getResourceAsStream("/res/octagonal_shape/backside2.png"));
             
-            shuffleArray(octagons);
+            for(int i=0; i<12; i++)
+            {
+                octagons[i] = new Octagon(name[i],i);
+            }
+
+            octagons[0].setX(300);
+            octagons[0].setY(140);
+
+            octagons[1].setX(396);
+            octagons[1].setY(140);
+
+            octagons[2].setX(300);
+            octagons[2].setY(215);
+
+            octagons[3].setX(396);
+            octagons[3].setY(215);
+
+            octagons[4].setX(300);
+            octagons[4].setY(290);
+
+            octagons[5].setX(396);
+            octagons[5].setY(290);
+
+            octagons[6].setX(300);
+            octagons[6].setY(365);
+
+            octagons[7].setX(390);
+            octagons[7].setY(365);
+
+            octagons[8].setX(204);
+            octagons[8].setY(215);
+
+            octagons[9].setX(492);
+            octagons[9].setY(215);
+
+            octagons[10].setX(204);
+            octagons[10].setY(290);
+
+            octagons[11].setX(492);
+            octagons[11].setY(290);
+
+            
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,22 +149,10 @@ public class OctagonManager {
         // g2.drawImage(backside2.image, 492, 215 , gp.octagonSize, gp.octagonSize, null);
         // g2.drawImage(backside2.image, 204, 290 , gp.octagonSize, gp.octagonSize, null);
         // g2.drawImage(backside2.image, 492, 290 , gp.octagonSize, gp.octagonSize, null);
-
-        g2.drawImage(backside.image, 300, 140 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 396, 140 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 300, 215 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 396, 215 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 300, 290 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 396, 290 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 300, 365 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 396, 365 , gp.octagonSize, gp.octagonSize, null);
-
-        g2.drawImage(backside.image, 204, 215 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 492, 215 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 204, 290 , gp.octagonSize, gp.octagonSize, null);
-        g2.drawImage(backside.image, 492, 290 , gp.octagonSize, gp.octagonSize, null);
-    
-        
+        for(int i=0; i<12; i++)
+        {
+            g2.drawImage(octagons[i].getPicture(), octagons[i].getX(), octagons[i].getY() , octagons[i].getWidth(), octagons[i].getHeight(), null);
+        }
         
     }
 }
