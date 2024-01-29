bb14cc97fd4bf93d0c533966d60a3254981070d0
duydinhthai <dinhthaiduy.tk10@gmail.com>
2024-01-29 08:17:45 +0700

    update number of players

--- ZickeZacke_test - Copy/Main/GamePanel.java
+++ ZickeZacke_test - Copy/Main/GamePanel.java
@@ -527,6 +527,8 @@ public class GamePanel extends JPanel implements Runnable, ActionListener , Mous
             if(mx >= 261 && mx<= 261+240){
                 if(my >= 303 && my<= 303+48){
                     howManyPlayer = 2;
+                    FeatherManager featherM = new FeatherManager(this);
+                    PLayerManager player = new PLayerManager(this);
                     gameState=playState;
                     playSE(0);
                     System.out.println(howManyPlayer);
@@ -535,6 +537,8 @@ public class GamePanel extends JPanel implements Runnable, ActionListener , Mous
             if(mx >= 261 && mx<= 261+240){
                 if(my >= 303+48 && my<= 303+48+48){
                     howManyPlayer = 3;
+                    FeatherManager featherM = new FeatherManager(this);
+                    PLayerManager player = new PLayerManager(this);
                     gameState=playState;
                     playSE(0);
                     System.out.println(howManyPlayer);
@@ -543,6 +547,8 @@ public class GamePanel extends JPanel implements Runnable, ActionListener , Mous
             if(mx >= 261 && mx<= 261+240){
                 if(my >= 303+48+48 && my<= 303+48+48+48){
                     howManyPlayer = 4;
+                    FeatherManager featherM = new FeatherManager(this);
+                    PLayerManager player = new PLayerManager(this);
                     gameState=playState;
                     playSE(0);
                     System.out.println(howManyPlayer);
